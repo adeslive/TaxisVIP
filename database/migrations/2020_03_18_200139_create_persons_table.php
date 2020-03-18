f573bbf3d480997ea470a3a4ac016336538d940d
Emigrobert <emigdiosanchez22@gmail.com>
2020-03-18 14:38:47 -0600

    Archivos del model

--- database/migrations/2020_03_18_200139_create_persons_table.php
+++ database/migrations/2020_03_18_200139_create_persons_table.php
@@ -14,7 +14,7 @@ class CreatePersonsTable extends Migration
     public function up()
     {
         Schema::create('persons', function (Blueprint $table) {
-            $table->bigIncrements('id');
+           $table->bigIncrements('id');
             $table->string('name', 45);
             $table->string('lastname', 45);
             $table->string('identity', 45);
