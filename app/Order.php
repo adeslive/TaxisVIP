3a1e08cd2e596c173edb38cd24b5846e11233423
Saulg96 <41869878+Saulg96@users.noreply.github.com>
2020-05-13 22:26:16 -0600

    editado

--- app/Order.php
+++ app/Order.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $primaryKey = 'id';
+    protected $table = 'Orders';
+    protected $fillable = ['price', 'distance', 'origin','destination','status'];
 }
