92c4a52649bc77dd0e95a48c3cbb8e192f59fdb8
Saulg96 <41869878+Saulg96@users.noreply.github.com>
2020-05-13 22:25:27 -0600

    editado

--- app/Driver.php
+++ app/Driver.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Driver extends Model
 {
+   protected $table = 'Drivers';
    protected $fillable = ['mileage', 'status', 'careerstatus', 'zones_id', 'persons_id'];
 }
