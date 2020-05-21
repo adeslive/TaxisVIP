85631588d49474cb33d2516d2b897ec76fad7198
robmndz97 <62403128+robmndz97@users.noreply.github.com>
2020-05-21 15:49:56 -0600

    Cambio en el archivo web para rutas de zonas

--- routes/web.php
+++ routes/web.php
@@ -54,6 +54,19 @@ Route::group(['prefix' => 'panel', 'middleware' => ['auth','access.level:admin|s
     Route::get('/carrera', 'OrderController@create')->name('carrera');
 });
 
+//ZONAS RUTAS DE PRUEBA
+
+Route::get('/zonas/agregarZona', function () {
+    return view('/zonas/agregarZona');
+})->name('agregarZona');
+
+Route::get('/zonas/listaZonas', function () {
+    return view('/zonas/listaZonas');
+})->name('lsitaZonas');
+
+Route::get('/zonas/zona', function () {
+    return view('/zonas/zona');
+})->name('zona');
 
 
 /*********************************MOVIL ROUTES************************************/
