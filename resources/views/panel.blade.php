ba5a2ef784a22385fb1c070984d0241690c75302
Victor <adeslive@outlook.es>
2020-05-26 13:02:51 -0600

    Cambios

--- resources/views/panel.blade.php
+++ resources/views/panel.blade.php
@@ -95,63 +95,10 @@
         </div>
     </div>
 </div>
-<div class="row">
-    <div class="col">
-        <div id="chart_div"></div>
-    </div>
-</div>
 @endsection
 
 @section('foot')
-<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-google.charts.load('current', {'packages':['corechart', 'bar']});
-      google.charts.setOnLoadCallback(drawStuff);
 
-      function drawStuff() {
-
-        var button = document.getElementById('change-chart');
-        var chartDiv = document.getElementById('chart_div');
-
-        var data = google.visualization.arrayToDataTable([
-            ['Chofer', 'Ventas', 'Multas'],
-            @foreach($drivers as $driver)
-                @php($sum = 0)
-                @foreach($driver->orders as $order)
-                    @php($sum += $order->price)
-                @endforeach
-            ['{{ $driver->person->name }}', {{ $sum }}, 0],
-            @endforeach
-        ]);
-
-        var materialOptions = {
-          width: '100%',
-          chart: {
-            title: 'Ventas por chofer'
-          },
-          series: {
-            0: { axis: 'ventas' }, // Bind series 0 to an axis named 'distance'.
-            1: { axis: 'multas' } // Bind series 1 to an axis named 'brightness'.
-          }
-        };
-
-        function drawMaterialChart() {
-          var materialChart = new google.charts.Bar(chartDiv);
-          materialChart.draw(data, google.charts.Bar.convertOptions(materialOptions));
-          button.innerText = 'Change to Classic';
-          button.onclick = drawClassicChart;
-        }
-
-        function drawClassicChart() {
-          var classicChart = new google.visualization.ColumnChart(chartDiv);
-          classicChart.draw(data, classicOptions);
-          button.innerText = 'Change to Material';
-          button.onclick = drawMaterialChart;
-        }
-
-        drawMaterialChart();
-    };
-    </script>
 <script>
     $(function () {
         $('[data-toggle="popover"]').click(()=>{
